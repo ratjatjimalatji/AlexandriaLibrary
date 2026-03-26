4e857d676d8eecf6422662c4a2fdb392a58a7978
Ratjatji Malatji <ratjatjmalatji@gmail.com>
2026-03-26 13:39:49 +0200

    Added TopAppBar w title & sign out and FloatingActionButton

--- app/src/main/java/com/example/libraryreader/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/libraryreader/screens/home/ReaderHomeScreen.kt
@@ -1,10 +1,116 @@
 package com.example.libraryreader.screens.home
 
+import android.R.attr.text
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.LibraryBooks
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.MenuBook
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.libraryreader.navigation.ReaderScreens
+import com.google.firebase.auth.FirebaseAuth
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun Home(navController: NavController) {
+    Scaffold(
+        topBar = {},
+        floatingActionButton = {
+            FABContent() {
+                navController.navigate(ReaderScreens.SearchScreen.name)
+            }
+        }) { innerPadding ->
+        Surface(
+            modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)){
+ReaderAppBar("Alexandria Library", navController = navController)
+        }
+    }
+}
 
 @Composable
-fun Home(navController: NavController){
-    Text(text= "welcome home big boy!!!")
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap() },
+        shape = RoundedCornerShape(50.dp),
+        containerColor = Color.Blue,
+        contentColor = Color.White
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a book to reading list",
+            tint = Color.White
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean = true,
+    navController: NavController
+) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(
+                        imageVector = Icons.Filled.MenuBook,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(25.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .scale(.6f)
+                    )
+                }
+                Text(text = title,
+                    color = Color.Blue.copy(alpha = 0.7f),
+                    style = TextStyle(fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(start = 5.dp))
+                Spacer(modifier = Modifier.width(150.dp))
+
+            }
+        },
+        actions = {
+            IconButton(onClick ={
+                FirebaseAuth.getInstance().signOut().run{
+                    navController.navigate(ReaderScreens.LoginScreen.name)
+                }
+            }){
+                Icon(imageVector = Icons.AutoMirrored.Filled.Logout,
+                    contentDescription = "Logout")
+
+            }
+        },
+        //colors = Color.Transparent)
+    )
 }
